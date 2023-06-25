657b87ea4ed4a118109cdf0cab2e624e1adade2f
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2023-06-25 21:23:07 +0530

    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+let dark_mode_toggle = document.querySelector('.dark-mode-switch')
+
+dark_mode_toggle.onclick = () => {
+    document.querySelector('body').classList.toggle('light')
+    document.querySelector('body').classList.toggle('dark')
+}
+
