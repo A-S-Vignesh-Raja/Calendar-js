739380f5173af69c4c2098836f0d3a2bb8f06d28
A.S.Vignesh Raja <136581635+A-S-Vignesh-Raja@users.noreply.github.com>
2023-06-28 19:52:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let calendar = document.querySelector('.calendar')
 const month_names =['January','February','March','April','May','June','July','August','September','October','November','December']
 
 let month_picker=document.querySelector("#month-picker")
+
 //genearte calendar
 
 month_picker.onclick=()=>{
